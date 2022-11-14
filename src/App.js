7182360355e4a3b8ae1027307880c40ab8eadc06
OmerutaPro <omeruta@OmerutaPros-MacBook-Pro.local>
2022-11-14 14:29:45 +0100

    pages folder, components and css per component created. Route and Browser created for each page with react router V6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//Page components
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/create' element={<Create/>}/>
+          <Route path='/search' element={<Search/>}/>
+          <Route path='/recipe/:id' element={<Recipe/>}/>
+        </Routes>
+      </BrowserRouter>
+    
     </div>
   );
 }
---
+++ src/pages/recipe/Recipe.js
@@ -0,0 +1,9 @@
+import "./Recipe.css"
+
+const Recipe = () => {
+  return (
+    <div>Recipe</div>
+  )
+}
+
+export default Recipe
